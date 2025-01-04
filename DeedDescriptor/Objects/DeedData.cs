2c9b6c3c85fb4dd4e73a72612db71c3dded742b6
Matthew Damiter <mattdamiter@gmail.com>
2025-01-04 01:11:22 -0500

    added deed description data entry form

---
+++ DeedDescriptor/Objects/DeedData.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace DeedDescriptor.Objects
+{
+    public class DeedData
+    {
+        public OwnerTaxID OwnerTaxID;
+
+        public decimal Area { get; set; }
+
+        public string? StreetName { get; set; }
+        public string? StreetWidth { get; set; }
+
+        public string? Township { get; set; }
+        public string? County{ get; set; }
+        public string? State { get; set; }
+
+        public string? ReferencePlan { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public Dictionary<Guid, OwnerTaxID> AdjoiningPropertyOwners {get;set;}
+
+        public DeedData()
+        {
+            OwnerTaxID = new OwnerTaxID();
+            AdjoiningPropertyOwners = new Dictionary<Guid, OwnerTaxID>();
+        }
+    }
+}
---
+++ DeedDescriptor/Objects/OwnerTaxID.cs
@@ -0,0 +1,16 @@
+﻿namespace DeedDescriptor.Objects
+{
+    public class OwnerTaxID
+    {
+        public string? Name { get; set; }
+        public bool IsTaxMap { get; set; }
+        public string? IDNumber { get; set; }
+
+        public void CopyData(OwnerTaxID ownerTaxID)
+        {
+            Name = ownerTaxID.Name;
+            IDNumber = ownerTaxID.IDNumber;
+            IsTaxMap = ownerTaxID.IsTaxMap;
+        }
+    }
+}
